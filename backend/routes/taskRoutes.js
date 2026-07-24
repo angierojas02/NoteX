644d044b6cf2aedcd20f3fe3cd68264fb2b509db
angierojas02 <223017952+angierojas02@users.noreply.github.com>
2026-07-24 20:39:32 -0300

    feat(taskroutes): add authentication and authorization middlewares to task routes

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,21 +1,20 @@
 import express from 'express'
 import Task from '../models/Tasks.js'
 import taskController from '../controllers/taskController.js'
+import { authorizeRoles } from '../middlewares/authRoje.js'
+import { authValidate } from '../middlewares/authHandler.js'
+
+const taskRouter = express.Router()
+
+taskRouter.get('/', authValidate, authorizeRoles('Admin','User'), taskController.getTasks)
+
+taskRouter.post('/', authValidate, authorizeRoles('Admin','User'), taskController.createNewTask)
+
+taskRouter.delete('/:id', authValidate, authorizeRoles('Admin','User'), taskController.deleteTask)
+
+taskRouter.patch ('/:id', authValidate, authorizeRoles('Admin','User'), taskController.updateStatus)
+
+taskRouter.put('/:id', authValidate, authorizeRoles('Admin','User'), taskController.updateTask)
 
 
-const router = express.Router()
-
-router.post('/', taskController.createNewTask)
-
-router.get('/', taskController.getTasks)
-
-router.get('/:id', taskController.getTaskById)
-
-router.delete('/:id', taskController.deleteTask)
-
-router.patch ('/:id', taskController.updateStatus)
-
-router.put('/:id', taskController.updateTask)
-
-
-export default router
+export default taskRouter
