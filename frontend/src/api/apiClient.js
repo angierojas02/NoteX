0cdd92639d20e110050cd091a942cf272ed2b608
angierojas02 <223017952+angierojas02@users.noreply.github.com>
2026-07-29 11:25:33 -0300

    feat(apiclient): create apiClient helper for HTTP requests

---
+++ frontend/src/api/apiClient.js
@@ -0,0 +1,45 @@
+
+export const apiClient = async (endpoint, options = {}) => {
+
+    const API_USERS = 'http://localhost:3000/users'
+
+    const defaultHeaders = {
+        'Content-type':'application/json',
+    }
+
+    const config = {
+        credentials: 'include',
+        ...options,
+        headers: {
+            ...defaultHeaders,
+            ...options.headers,
+        }
+    }
+
+    try {
+        const response = await fetch(`${API_USERS}${endpoint}`,config)
+
+        const isJson = response.headers.get('content-type')?.includes('application/json')
+                const data = isJson ? await response.json() : null
+
+                if (!response.ok) {
+                    const errorMessage = 
+                    data?.error?.message ||
+                    data?.message ||
+                    `Server error (${response.status})`
+
+                    const error = new Error(errorMessage)
+                    error.status = response.status
+                    error.details = data
+                    throw error
+                }
+            return data
+    } catch (error) {
+        if (error.name === 'TypeError' && error.message.includes('fetch')) {
+                    console.error('Red error ', error)
+                    throw new Error('Could not connect to the server. Check your internet connection.', {cause: error})
+        }
+        console.error('Login error ', error)
+        throw error 
+    }
+}
