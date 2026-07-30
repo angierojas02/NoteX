cea411394559845f19d88c9e38f7652d95fb0c01
angierojas02 <223017952+angierojas02@users.noreply.github.com>
2026-07-30 18:04:49 +0000

    fix: correct the URL to the backend

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,7 +1,7 @@
 
 export const apiClient = async (endpoint, options = {}) => {
 
-    const BASE_URL = import.meta.env.VITE_API_URL
+    const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000"
     const API_USERS = `${BASE_URL}/users`
 
     const defaultHeaders = {
