f9145bfc37265ea47698cd6902f7e027527dd7c4
angierojas02 <223017952+angierojas02@users.noreply.github.com>
2026-07-30 17:05:30 +0000

    feat(hooks): prepare frontend for production deployment

--- frontend/src/hooks/useTasks.js
+++ frontend/src/hooks/useTasks.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 
-const API_URL = 'http://localhost:3000/tasks'
+const API_URL = import.meta.env.VITE_API_URL
 
 export function useTasks () {
   const [tasks, setTasks] = useState([])
