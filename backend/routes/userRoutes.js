f3e5d99290ca30b3c9a08c8ba98fe29969d1b729
angierojas02 <223017952+angierojas02@users.noreply.github.com>
2026-07-30 13:09:40 -0300

    feat(routes): add authValidate and authorizeRoles middlewares to taskRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,19 @@
 import express from 'express'
 import User from '../models/User.js'
-import authController from '../controllers/authController.js'
-
+import UsersController from '../controllers/UsersController.js'
+import { authorizeRoles } from '../middlewares/authRole.js'
+import { authValidate } from '../middlewares/authHandler.js'
+ 
 
 const userRouter = express.Router()
 
-userRouter.get('/', authController.getAllUsers)
-userRouter.post('/register', authController.registerUser)
-userRouter.post('/login', authController.loginUser)
+userRouter.get('/admin', authValidate, authorizeRoles('Admin'), UsersController.getAllUsers)
+userRouter.get('/verify', authValidate, UsersController.verifyToken)
+userRouter.post('/register', UsersController.registerUser)
+userRouter.post('/login', UsersController.loginUser)
+userRouter.put('/:id',authValidate, authorizeRoles('Admin'), UsersController.modifyUser)
+userRouter.delete('/:id', authValidate, authorizeRoles('Admin'), UsersController.deleteUser)
+userRouter.post('/logout', UsersController.logoutUser)
 
 export default userRouter
 
