34688b005c4d443750e6b769496c75f2abafe457
angierojas02 <223017952+angierojas02@users.noreply.github.com>
2026-07-31 13:45:08 +0000

    fix: correct the BASE_URL with the link of the backend

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,7 +1,7 @@
 
 export const apiClient = async (endpoint, options = {}) => {
 
-    const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000"
+    const BASE_URL = import.meta.env.VITE_API_URL || 'https://notex-backend-vgmu.onrender.com'
     const API_USERS = `${BASE_URL}/users`
 
     const defaultHeaders = {
