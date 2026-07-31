2f7fd2f47e4bdd94c868ec07a26b1757eaf66079
angierojas02 <223017952+angierojas02@users.noreply.github.com>
2026-07-31 13:38:43 +0000

    fix: correct CORS line to allow the connection to the frontend

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173', 
+  origin: process.env.FRONTEND_URL || 'https://notex-frontend.vercel.app', 
   credentials: true                
 }))
 
